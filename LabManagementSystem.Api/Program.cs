41ea568cc8f3711054ae1155c540a506a8383777
Renu Kontham <renu.munugoti@gmail.com>
2022-11-26 19:24:49 +0530

    Key vault configuration

--- LabManagementSystem.Api/Program.cs
+++ LabManagementSystem.Api/Program.cs
@@ -1,13 +1,17 @@
 using LabManagementSystem.Data;
 using LabManagementSystem.Data.Providers;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string dbConnString = GetConnectionString();
 // Add services to the container.
 builder.Services.AddTransient<ILabMgmtSystemProvider, LabMgmtSystemProvider>();
-builder.Services.AddDbContext<LabMgmtDbContext>(item => 
-item.UseSqlServer("Server=tcp:socgensqlserver.database.windows.net,1433;Initial Catalog=Capstone-rk;Persist Security Info=False;User ID=socgen-admin;Password=Pa$$w0rd123$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+builder.Services.AddDbContext<LabMgmtDbContext>(item =>
+item.UseSqlServer(dbConnString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,3 +33,22 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static string GetConnectionString()
+{
+    SecretClientOptions options = new SecretClientOptions()
+    {
+        Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+    };
+    var client = new SecretClient(new Uri("https://capstone-rk-dbkey.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+    KeyVaultSecret secret = client.GetSecret("db-conn-string");
+
+    return secret.Value;
+}
